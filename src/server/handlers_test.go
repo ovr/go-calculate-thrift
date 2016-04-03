9140334b6edb844b43d721c2abdf2bb5a9cb9655
Dmitry Patsura <zaets28rus@gmail.com>
2016-04-03 18:58:59 +0300

    [Server\Tests] Implement tests for minus

--- src/server/handlers_test.go
+++ src/server/handlers_test.go
@@ -34,3 +34,34 @@ func TestPlus(t *testing.T) {
 		assert.Equal(t, testsFixture.expected, result, "they should be equal")
 	}
 }
+
+var dataProviderMinusTests = []struct {
+	a        int32 // inputA
+	b        int32 // inputB
+	expected int32 // expected result
+}{
+	{25, 5, 20},
+	{2, 1, 1},
+	{25, 50, -25},
+	{0, 0, 0},
+	{25, 0, 25},
+	{-25, 0, -25},
+	{-1, -1, 0},
+	{-1, 1, -2},
+}
+
+func TestMinus(t *testing.T) {
+	var (
+		result int32
+		err    error
+	)
+
+	service := NewCalculatorHandler()
+
+	for _, testsFixture := range dataProviderMinusTests {
+		result, err = service.Minus(testsFixture.a, testsFixture.b)
+
+		assert.Equal(t, nil, err, "err should be nil")
+		assert.Equal(t, testsFixture.expected, result, "they should be equal")
+	}
+}
