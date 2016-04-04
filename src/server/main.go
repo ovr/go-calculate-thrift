0ae1cc7c2ec07e0e76421f97543db2a074ade354
Dmitry Patsura <zaets28rus@gmail.com>
2016-04-04 21:53:08 +0300

    [Server] Server is using godeps now

--- src/server/main.go
+++ src/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/ovr/go-calculate-thrift/gen-go/tutorial"
 	"log"
 	"os"
